75f494c780632f2cd0eeab1b12846b2a68a1b909
Natalya Malyushenko <natamalyushenko@gmail.com>
2024-07-19 23:01:13 +0300

    homepage created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
+import Image from 'next/image'
+
+function Home() {
   return (
-    <main>
-      <h1>Hello world</h1>
+    <main className="flex items-center justify-center flex-grow max-w-[1000px] my-0 mx-auto lg:flex-col lg:gap-5 ">
+      <h1 className="shrink text-left lg:text-center">
+        Evercode Lab
+        <br /> Объединяем бизнес и технологии
+      </h1>
+      <Image
+        src="/main.svg"
+        alt="Иллюстрация командной работы и технических процессов"
+        width="850"
+        height="500"
+        className="w-8/12 lg:w-10/12 lg:my-4"
+      />
     </main>
   )
 }
+
+export default Home
