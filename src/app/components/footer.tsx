38a1aa46aafec3eb7abbaed6f12b8821504f040a
Natalya Malyushenko <natamalyushenko@gmail.com>
2024-07-19 23:02:47 +0300

    footer created

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,5 +1,9 @@
 function Footer() {
-  return <div>footer</div>
+  return (
+    <footer className="mt-auto py-5 px-7 border-t text-center text-slate-400">
+      <small>&copy; 2013-2024 Evercode Lab</small>
+    </footer>
+  )
 }
 
 export default Footer
