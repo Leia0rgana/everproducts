3023b1fcfbcb50689b25e32e49bac898dfa20635
Natalya Malyushenko <natamalyushenko@gmail.com>
2024-07-21 11:25:47 +0300

    image wrapped in suspense component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,23 @@
 import Image from 'next/image'
+import { Suspense } from 'react'
+import Loading from './loading'
 
 function Home() {
   return (
     <main className="flex items-center justify-center flex-grow max-w-[1000px] my-0 mx-auto lg:flex-col lg:gap-5 ">
-      <h1 className="shrink text-left lg:text-center">
+      <h1 className="shrink">
         Evercode Lab
         <br /> Объединяем бизнес и технологии
       </h1>
-      <Image
-        src="/main.svg"
-        alt="Иллюстрация командной работы и технических процессов"
-        width="850"
-        height="500"
-        className="w-8/12 lg:w-10/12 lg:my-4"
-      />
+      <Suspense fallback={<Loading />}>
+        <Image
+          src="/main.svg"
+          alt="Иллюстрация командной работы и технических процессов"
+          width="850"
+          height="500"
+          className="w-8/12 lg:w-10/12 lg:my-4"
+        />{' '}
+      </Suspense>
     </main>
   )
 }
