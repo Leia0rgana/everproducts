397b6228bc71ea54109ae67e81d54451b6d821da
Natalya Malyushenko <natamalyushenko@gmail.com>
2024-07-21 11:25:14 +0300

    now dropdown menu closes when screen size increases

--- src/app/components/UI/navbar.tsx
+++ src/app/components/UI/navbar.tsx
@@ -2,10 +2,11 @@
 
 import Link from 'next/link'
 import MenuButton from './menu-button'
-import { selectIsButtonClicked } from '@/redux/slices/buttonSlice'
-import { useAppSelector } from '@/redux/hooks'
+import { selectIsButtonClicked, toggleButton } from '@/redux/slices/buttonSlice'
+import { useAppSelector, useAppDispatch } from '@/redux/hooks'
 import Image from 'next/image'
 import { usePathname } from 'next/navigation'
+import { useRef, useEffect } from 'react'
 
 const navLinks = [
   { title: 'Продукты', path: '/products' },
@@ -17,10 +18,28 @@ const navLinks = [
 
 function Navbar() {
   const isClicked = useAppSelector(selectIsButtonClicked)
+  const dispatch = useAppDispatch()
   const pathname = usePathname()
+  const pathnameRef = useRef(pathname)
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768 && isClicked === true)
+        dispatch(toggleButton())
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    if (isClicked && pathname !== pathnameRef.current) {
+      dispatch(toggleButton())
+      pathnameRef.current = pathname
+    }
+
+    return () => window.removeEventListener('resize', handleResize)
+  }, [pathname, dispatch, isClicked])
 
   return (
-    <nav className="flex flex-row justify-between items-center my-10 mx-5 gap-5">
+    <nav className="flex flex-row justify-between items-center mt-10 mx-5 gap-5 xs:mb-0">
       <Link href={'/'}>
         <Image
           src="/logo-header.svg"
@@ -33,17 +52,22 @@ function Navbar() {
       <ul
         className={`flex flex-row justify-end items-center gap-10 ${
           isClicked
-            ? ' xs:left-8 xs:w-auto md:flex-col md:gap-4 md:h-auto md:absolute md:top-[100px] md:right-8 md:w-2/5 backdrop-blur-lg md:border md:border-slate-300/30 md:rounded-lg'
+            ? 'md:flex-col md:gap-1 md:h-1/3 md:absolute md:top-[100px] md:right-8 md:w-2/5 md:bg-slate-50/80 md:backdrop-blur-lg md:border md:border-slate-300/30 md:rounded-lg md:min-h-60 xs:left-8 xs:w-auto xs:gap-0'
             : 'md:hidden'
         }`}
       >
         {navLinks.map((navItem) => (
           <li
             key={navItem.path}
-            className={` relative py-1 after:content-['_'] after:bg-bright-green after:h-[3px] after:w-0 after:left-0 after:absolute after:transition-[width] after:duration-300 after:ease-in-out after:bottom-0 hover:after:w-full 
-            ${pathname === navItem.path && 'after:w-full'}`}
+            className={`w-full h-full relative py-1 after:content-['_'] after:bg-bright-green after:h-[3px] after:w-0 after:left-0 after:absolute after:transition-[width] after:duration-300 after:ease-in-out after:bottom-0 hover:after:w-full md:hover:bg-slate-200/70 md:after:content-none
+            ${pathname === navItem.path && 'after:w-full md:bg-slate-200/70'}`}
           >
-            <Link href={navItem.path}>{navItem.title}</Link>
+            <Link
+              href={navItem.path}
+              className="uppercase md:normal-case flex justify-center items-center w-full h-full text-center text-dark-blue font-medium"
+            >
+              {navItem.title}
+            </Link>
           </li>
         ))}
       </ul>
