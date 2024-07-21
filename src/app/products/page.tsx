28a71ed83b505adff5a0cdd6ade66b8a11ca7cbd
Natalya Malyushenko <natamalyushenko@gmail.com>
2024-07-22 01:30:16 +0300

    integrated form into page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,15 +1,23 @@
 import Image from 'next/image'
+import type { Metadata } from 'next'
 import { Suspense } from 'react'
 import Loading from '../loading'
 import Card from '../components/UI/card'
 import { PRODUCTS } from '@/data/products'
 import { SlArrowDown } from 'react-icons/sl'
 import Link from 'next/link'
+import Modal from '../components/UI/modal'
+import ContactUsForm from '../components/contact-us-form'
+
+export const metadata: Metadata = {
+  title: 'Продукты | Evercode Lab',
+  description: 'Коробочные IT-решения для вашего бизнеса',
+}
 
 function Products() {
   return (
-    <main className=" text-dark-blue flex flex-col max-w-[1000px] my-0 mx-auto lg:mt-16">
-      <div className="flex items-center h-screen lg:flex-col lg:h-auto">
+    <main className=" text-dark-blue flex flex-col max-w-[1000px] my-0 mx-auto">
+      <div className="flex items-center h-[calc(100vh_-_117px)] lg:flex-col lg:h-auto lg:mb-12 xs:mb-0 xs:h-screen">
         <section className="shrink flex flex-col gap-3 lg:items-center">
           <h1>Наши продукты</h1>
           <p className="mx-2 text-lg leading-relaxed lg:text-center">
@@ -29,17 +37,26 @@ function Products() {
             alt="Разработка продуктов"
             width="850"
             height="500"
-            className="w-full"
+            className="w-full px-3"
           />{' '}
         </Suspense>
       </div>
       <div
         id="products"
-        className="grid grid-cols-[repeat(3,_350px)] gap-8 mx-3 mb-8 justify-center xl:grid-cols-[repeat(3,_300px)] lg:grid-cols-[repeat(2,_330px)] lg:mt-12 md:grid-cols-[repeat(1,_350px)] xs:grid-cols-[repeat(1,300px)]"
+        className="grid grid-cols-[repeat(3,_310px)] gap-8 mx-3 mb-8 justify-center xl:grid-cols-[repeat(3,_300px)] lg:grid-cols-[repeat(2,_330px)] lg:mt-12 md:grid-cols-[repeat(1,_350px)] xs:grid-cols-[repeat(1,300px)]"
       >
         {PRODUCTS.map((product) => (
           <Card productInfo={product} key={product.title} />
         ))}
+      </div>
+      <div className="border-t mx-5 mt-auto py-5 px-7 flex justify-center items-center gap-10">
+        <h2>Хотите узнать больше?</h2>
+        <p className="sm:hidden">
+          Напишите нам сейчас, чтобы получить доступ к демо-версии Everchain SDK
+        </p>
+        <Modal>
+          <ContactUsForm />
+        </Modal>
       </div>
     </main>
   )
