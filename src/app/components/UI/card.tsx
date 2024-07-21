9b3460fb80267783464f1d23d6de8ce04d2e8801
Natalya Malyushenko <natamalyushenko@gmail.com>
2024-07-21 11:27:00 +0300

    created component to hold product info

---
+++ src/app/components/UI/card.tsx
@@ -0,0 +1,24 @@
+import { IProduct } from '@/data/products'
+import Image from 'next/image'
+
+function Card({ productInfo }: { productInfo: IProduct }) {
+  return (
+    <div className="flex flex-col p-3 bg-slate-50 border rounded-md hover:shadow-lg hover:scale-105 transition-transform">
+      <Image
+        src={productInfo.image.src}
+        alt={productInfo.image.alt}
+        width="315"
+        height="192"
+        className="w-fit my-4"
+      />
+      <p className="p-1 text-xl leading-relaxed font-medium">
+        {productInfo.title}
+      </p>
+      <p className="p-1 my-1 text-sm leading-relaxed">
+        {productInfo.description}
+      </p>
+    </div>
+  )
+}
+
+export default Card
