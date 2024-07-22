c19e080f944845f2d5a8abd50a926034a4f728f2
Natalya Malyushenko <natamalyushenko@gmail.com>
2024-07-22 14:08:14 +0300

    added ids to products

--- src/data/products.ts
+++ src/data/products.ts
@@ -5,9 +5,20 @@ export interface IProduct {
     alt: string
     src: string
   }
+  id: string
 }
 
 export const PRODUCTS: Readonly<Array<IProduct>> = [
+  {
+    title: 'Everchain SDK',
+    description:
+      'Everchain SDK позволяет интегрировать более 50 криптовалют и 10000 токенов в некастодиальный криптокошелек. Благодаря API SDK вы получаете все пользовательские сценарии для любой монеты или токена',
+    image: {
+      alt: 'Некастодиальный криптокошелек',
+      src: 'https://evercodelab.com/ru/img/product-front/SDK/SDK.svg',
+    },
+    id: 'everchain-sdk',
+  },
   {
     title: 'Evercode Online Medic',
     description:
@@ -16,6 +27,7 @@ export const PRODUCTS: Readonly<Array<IProduct>> = [
       alt: 'Телемедицина',
       src: 'https://evercodelab.com/ru/img/product-front/Medic/Medic.svg',
     },
+    id: 'evercode-online-medic',
   },
 
   {
@@ -23,27 +35,21 @@ export const PRODUCTS: Readonly<Array<IProduct>> = [
     description:
       'Marketplace - IT-решение для электронной коммерции с интуитивно понятным интерфейсом и высоким уровнем защиты',
     image: {
-      alt: 'Телемедицина',
-      src: 'https://evercodelab.com/ru/img/product-front/Medic/Medic.svg',
+      alt: 'Электронная коммерция',
+      src: 'https://evercodelab.com/ru/img/product-front/Evermarket/Evermarket.svg',
     },
+    id: 'evermarket',
   },
-  {
-    title: 'Everchain SDK',
-    description:
-      'Everchain SDK позволяет интегрировать более 50 криптовалют и 10000 токенов в некастодиальный криптокошелек. Благодаря API SDK вы получаете все пользовательские сценарии для любой монеты или токена',
-    image: {
-      alt: 'Телемедицина',
-      src: 'https://evercodelab.com/ru/img/product-front/Medic/Medic.svg',
-    },
-  },
+
   {
     title: 'Everchain White Label Wallet',
     description:
       'Everchain White Label Wallet позволяет запустить полностью готовый некастодиальный криптокошелек под вашим брендом за 5-7 рабочих дней',
     image: {
-      alt: 'Телемедицина',
-      src: 'https://evercodelab.com/ru/img/product-front/Medic/Medic.svg',
+      alt: 'Некастодиальный криптокошелек',
+      src: 'https://evercodelab.com/ru/img/product-front/Wallet/Wallet.svg',
     },
+    id: 'wallet',
   },
   {
     title: 'EverCourse',
@@ -51,16 +57,18 @@ export const PRODUCTS: Readonly<Array<IProduct>> = [
       'EverCourse - платформа для онлайн-обучения, удобная как для преподавателей, так и для обучающихся',
     image: {
       alt: 'Телемедицина',
-      src: 'https://evercodelab.com/ru/img/product-front/Medic/Medic.svg',
+      src: 'https://evercodelab.com/ru/img/product-front/Course/Course.svg',
     },
+    id: 'evercourse',
   },
   {
     title: 'Evervault',
     description:
       'Evervault помогает бизнесам безопасно хранить и использовать секретные данные',
     image: {
-      alt: 'Телемедицина',
-      src: 'https://evercodelab.com/ru/img/product-front/Medic/Medic.svg',
+      alt: 'Разработка продуктов',
+      src: '/products.svg',
     },
+    id: 'evervault',
   },
 ]
